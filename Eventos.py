57bcdcdeb51c7a9677925a1af8602eea1032a228
Carlos Cancinos <61300585+carloscancinos12@users.noreply.github.com>
2022-05-06 11:14:32 -0600

    self eventos

--- Eventos.py
+++ Eventos.py
@@ -7,69 +7,93 @@ from tkcalendar import *
 from tkinter import ttk
 from Asistencia import AsisForm
 
-def EvtForm(pw):
+class EvtForm:
+    
+    def __init__(self,pw):
+        self.principal = pw
+        self.w = Frame(pw,width=1200,height=675,bg='#707070')
+        self.w.place(x=0, y=0)
+        self.fuenteG = font=('Comic Sans MS', 19,'bold')
+        self.fuenteP = font=('Comic Sans MS', 15,'bold')
+        self.bglabel = '#707070'
+        self.fglabel = '#FFFFFF'
+        self.posx = 80
+        self.img1 = ImageTk.PhotoImage(Image.open('Images/HM.png')) 
+        Button(self.w, command = self.cmd, image=self.img1, border=0,activebackground='#000000', bg='#707070').place(x=5, y = 10)
+
+        self.lab('Eventos', self.fuenteG, self.bglabel, self.fglabel, 490, 10)
+        self.lab('Datos Evento', self.fuenteG, self.bglabel, self.fglabel, self.posx, 55)
+        self.lab('Nombre.', self.fuenteP, self.bglabel, self.fglabel, self.posx, 95)
+        self.lab('Fecha', self.fuenteP, self.bglabel, self.fglabel, self.posx, 130)
+        self.lab('Hora (HH:MM)', self.fuenteP, self.bglabel, self.fglabel, self.posx, 165)
+        self.lab('Lugar', self.fuenteP, self.bglabel, self.fglabel, self.posx, 200)
+        self.lab('Eventos: ', self.fuenteG, self.bglabel, self.fglabel, 100, 300 )
+        
+        self.name = StringVar()
+        self.LugarE = StringVar()
+        self.HoraE= StringVar()
+        self.nombre = self.Ent(self.name, 156, 175, 105)
+        self.hora = self.Ent(self.HoraE, 143, 250, 175)
+        self.lugar = self.Ent(self.LugarE, 155, 175, 210)
+        self.cal=DateEntry(self.w,width=30)
+        self.cal.place(x=175,y=140)
+
+        self.guardar = self.btn(self.w, 980, 275, 'guardar', '#000000', '#FF4e10', self.Sav,'Arial', 12,'bold')
+        self.editar = self.btn(self.w, 980, 600, 'editar', '#000000', '#FF4e10', self.edit,'Arial', 12,'bold')
+        self.borrar = self.btn(self.w, 750, 600, 'borrar', '#000000', '#FF4e10', self.delete,'Arial', 12,'bold')
+        self.asistencia = self.btn(self.w, 100, 600, 'Asistencia', '#000000', '#FF4e10', self.Asistencia,'Arial', 12,'bold')
+
+        #Agenda en tabla
+        self.tabladata = ttk.Treeview(self.w)
+        self.tabladata=ttk.Treeview(self.w,columns=("col1","col2","col3","col4"))
+        self.tabladata.column("#0", width=80)
+        self.tabladata.column("col1",width=360, anchor=CENTER)
+        self.tabladata.column("col2",width=100, anchor=CENTER)
+        self.tabladata.column("col3",width=100, anchor=CENTER)
+        self.tabladata.column("col4",width=100, anchor=CENTER)
+        self.tabladata.heading("#0",text="Id",anchor=CENTER)
+        self.tabladata.heading("col1",text="Evento",anchor=CENTER)
+        self.tabladata.heading("col2",text="Fecha",anchor=CENTER)
+        self.tabladata.heading("col3",text="Hora",anchor=CENTER)
+        self.tabladata.heading("col4",text="Lugar",anchor=CENTER)
+        self.tabladata.place(x=230,y=340)
+
+    def Sav(self):
+        None
+        
+    def cmd(self):
+        self.w.destroy()
+        
+    def edit(self):
+        None
+        
+    def delete(self):
+        None
+        
+    def Asistencia(self):
+        AsisForm(self.principal, 1)
     
 
-    w = Frame(pw,width=1200,height=675,bg='#707070')
-    w.place(x=0, y=0)
-    fuenteG = font=('Comic Sans MS', 19,'bold')
-    fuenteP = font=('Comic Sans MS', 15,'bold')
-    bglabel = '#707070'
-    fglabel = '#FFFFFF'
-    posx = 80
-
-    def Sav():
-        None
-        
-    def cmd():
-        w.destroy()
-        
-    def edit():
-        None
-        
-    def delete():
-        None
-        
-    def Asistencia():
-        AsisForm(pw, 1)
-        
-    global img1
-    img1 = ImageTk.PhotoImage(Image.open('Images/HM.png')) 
-    Button(w, command = cmd, image=img1, border=0,activebackground='#000000', bg='#707070').place(x=5, y = 10)
-
     #Labels formulario
-    def lab(text, font, bg, fg, x, y):
-        labe = Label(w,text=text, font=font, bg=bg, foreground=fg)
+    def lab(self, text, font, bg, fg, x, y):
+        labe = Label(self.w,text=text, font=font, bg=bg, foreground=fg)
         labe.pack()
         labe.place(x=x, y=y)
 
 
-    lab('Eventos', fuenteG, bglabel, fglabel, 490, 10)
-    lab('Datos Evento', fuenteG, bglabel, fglabel, posx, 55)
-    lab('Nombre.', fuenteP, bglabel, fglabel, posx, 95)
-    lab('Fecha', fuenteP, bglabel, fglabel, posx, 130)
-    lab('Hora (HH:MM)', fuenteP, bglabel, fglabel, posx, 165)
-    lab('Lugar', fuenteP, bglabel, fglabel, posx, 200)
-    lab('Eventos: ', fuenteG, bglabel, fglabel, 100, 300 )
-    name = StringVar()
-    Lugar = StringVar()
-    Hora= StringVar()
+    
 
-    def Ent(textvar, width, x, y):
-        Entr = Entry(w,textvariable=textvar, width=width)
+    def Ent(self, textvar, width, x, y):
+        Entr = Entry(self.w,textvariable=textvar, width=width)
         Entr.pack()
         Entr.place(x=x, y=y)
         
 
-    Ent(name, 156, 175, 105)
-    Ent(Hora, 143, 250, 175)
-    Ent(Lugar, 155, 175, 210)
-    cal=DateEntry(w,width=30)
-    cal.place(x=175,y=140)
+    
     
    
 
-    def btn(f1, x, y, text, bcolor, fcolor, command, font, siz, tipe):
+    def btn(self, f1, x, y, text, bcolor, fcolor, command, font, siz, tipe):
         #Botones para menu
         def on_enter(e):
             buttons['background'] = bcolor
@@ -80,28 +104,12 @@ def EvtForm(pw):
             buttons['foreground'] = bcolor
             
             
-        buttons = Button(w, width=18, height=2, text= text, fg  = bcolor, bg=fcolor, command=command, border=0, activebackground=bcolor, activeforeground=fcolor,font=(font, siz, tipe))
+        buttons = Button(self.w, width=18, height=2, text= text, fg  = bcolor, bg=fcolor, command=command, border=0, activebackground=bcolor, activeforeground=fcolor,font=(font, siz, tipe))
         buttons.bind("<Enter>", on_enter)
         buttons.bind("<Leave>", on_leave)
         buttons.place(x=x, y=y)
     
-    btn(w, 980, 275, 'guardar', '#000000', '#FF4e10', Sav,'Arial', 12,'bold')
-    btn(w, 980, 600, 'editar', '#000000', '#FF4e10', edit,'Arial', 12,'bold')
-    btn(w, 750, 600, 'borrar', '#000000', '#FF4e10', delete,'Arial', 12,'bold')
-    btn(w, 100, 600, 'Asistencia', '#000000', '#FF4e10', Asistencia,'Arial', 12,'bold')
     
     
-    #Agenda en tabla
-    tabladata = ttk.Treeview(w)
-    tabladata=ttk.Treeview(w,columns=("col1","col2","col3","col4"))
-    tabladata.column("#0", width=80)
-    tabladata.column("col1",width=360, anchor=CENTER)
-    tabladata.column("col2",width=100, anchor=CENTER)
-    tabladata.column("col3",width=100, anchor=CENTER)
-    tabladata.column("col4",width=100, anchor=CENTER)
-    tabladata.heading("#0",text="Id",anchor=CENTER)
-    tabladata.heading("col1",text="Evento",anchor=CENTER)
-    tabladata.heading("col2",text="Fecha",anchor=CENTER)
-    tabladata.heading("col3",text="Hora",anchor=CENTER)
-    tabladata.heading("col4",text="Lugar",anchor=CENTER)
-    tabladata.place(x=230,y=340)
+    
+    
