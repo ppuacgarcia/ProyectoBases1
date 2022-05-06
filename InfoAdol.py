0c72b420d93aa8482e7c80055672cc387770993b
ppuacgarcia <55265850+ppuacgarcia@users.noreply.github.com>
2022-05-06 11:40:50 -0600

    cambios
    update para colabs

--- InfoAdol.py
+++ InfoAdol.py
@@ -142,8 +142,8 @@ class adolinfo:
             conn=mariadb.connect(
                 host="localhost",
                 user="root",
-                #password="123456789",
-                password="Kamado_Tanjiro_12",
+                password="123456789",
+                #password="Kamado_Tanjiro_12",
                 database="iglesia",
                 autocommit=True
             )
